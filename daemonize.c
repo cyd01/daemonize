e85df09026a4e7380735bae551c888e993fda518
Cyd <34154246+cyd01@users.noreply.github.com>
2024-09-11 17:18:14 +0200

    Update daemonize.c

--- daemonize.c
+++ daemonize.c
@@ -6,7 +6,26 @@
 #include <unistd.h>
 #include <time.h>
 
+/* getopt stuff */
+extern char * optarg;
+extern int optind, opterr;
+
+/* Waiting time (in seconds) before starting */
+int debug_flag=0;
+int sleeping_time=0 ;
+char *waiting_time=NULL;
+static void usage(char* program, int exitcode) ;
+
 void daemonize (char * const *cmd) {
+    if( debug_flag ) {
+        fprintf(stderr,"DEBUG: start daemon command <%s",cmd[0]);
+        int i=1;
+        while( cmd[i]!=NULL ) {
+            fprintf(stderr," %s",cmd[i]);
+            i++;
+        }
+        fprintf(stderr,">\n");
+    }
     int pid ;
     if( cmd==NULL ){
         fprintf(stderr,"No command found");
@@ -40,18 +59,14 @@ void daemonize (char * const *cmd) {
 
     /* child B (grandchild) */
     /* Get parameters */
-    if( getenv("DAEMONIZE_SLEEP")!=NULL ) {
-        int wait_time = -1 ;
-        if( sscanf(getenv("DAEMONIZE_SLEEP"),"%d",&wait_time)>0 ) {
-            if( wait_time>=0 ) {
-                sleep(wait_time);
-            }
-        }
+    if( sleeping_time>=0 ) { /* waiting for sleeping_time seconds */
+        sleep(sleeping_time);
     }
-    if( getenv("DAEMONIZE_TIME")!=NULL ) {
+
+    if( waiting_time!=NULL ) { /* waiting for time is waiting_time */
         int wait_hour = -1 ;
         int wait_min = -1 ;
-        if( sscanf(getenv("DAEMONIZE_TIME"),"%d:%d",&wait_hour,&wait_min)>0 ) {
+        if( sscanf(waiting_time,"%d:%d",&wait_hour,&wait_min)>0 ) {
             if( (wait_hour>=0) && (wait_hour<24) && (wait_min>=0) && (wait_min<60) ) {
                 struct tm * tm;
                 time_t t = 0 ;
@@ -75,14 +90,62 @@ void daemonize (char * const *cmd) {
 }
 
 int main(int argc, char **argv, char **arge) {
-    if( argc==1 ) {
-        fprintf(stderr, "Usage: %s Command [Options...]\n",basename(argv[0])) ;
-        fprintf(stderr, "\n");
-        fprintf(stderr, "Environment variables:\n");
-        fprintf(stderr, "    DAEMONIZE_TIME=HH:MM wait for a specific time before starting command\n");
-        fprintf(stderr, "    DAEMONIZE_SLEEP=SS wait for SS seconds before starting command\n");
+    int opt;
+    if( getenv("DAEMONIZE_SLEEP")!=NULL ) {
+        int sleep_time = -1 ;
+        if( sscanf(getenv("DAEMONIZE_SLEEP"),"%d",&sleep_time)>0 ) {
+            if( sleep_time>=0 ) {
+                sleeping_time=sleep_time;
+            }
+        }
+    }
+    if( getenv("DAEMONIZE_TIME")!=NULL ) {
+        waiting_time=(char*)malloc(strlen(getenv("DAEMONIZE_TIME"))+1);
+        strcpy(waiting_time,getenv("DAEMONIZE_TIME"));
+    }
+
+    while ((opt=getopt(argc, argv, "dhs:t:"))!=EOF) {
+      switch (opt) {
+        case 'd': debug_flag=1 ; break ;
+        case 'h': usage(basename(argv[0]),0) ; break ;
+        case 's': 
+          int sleep_time = -1 ;
+          if( sscanf(optarg,"%d",&sleep_time)>0 ) {
+              if( sleep_time>=0 ) {
+                  sleeping_time=sleep_time;
+                  if( debug_flag ) { fprintf(stderr,"DEBUG: set sleeping time to <%d>\n",sleeping_time); }
+              }
+          }
+          break ;
+        case 't': 
+          waiting_time=(char*)malloc(strlen(optarg)+1);
+          strcpy(waiting_time,optarg);
+          if( debug_flag ) { fprintf(stderr,"DEBUG: set waiting time to <%s>\n",waiting_time); }
+          break ;
+        default: fprintf(stderr,"Wrong parameter\n"); usage(basename(argv[0]),1) ;
+      }
+    }
+
+    if( argc<=optind ) {
+        fprintf(stderr,"Not enought parameters, missing command\n");
+        usage(basename(argv[0]),1) ;
     } else {
-        daemonize( (char * const *)&argv[1] ) ;
+        daemonize( (char * const *)&argv[optind] ) ;
     }
     return 0 ;
+}
+
+static void usage(char *program, int exitcode) {
+  fprintf(stderr, 
+    "Usage: %s [-options...] Command [Parameters...]\n"
+    "  start Command as a daemon\n"
+    "\t-s <sec>    sleeping before starting (in seconds)\n"
+    "\t-t <HH:MM>  waiting time before starting (format HH:MM)\n"
+    "\n"
+    "Environment variables:\n"
+    "\tDAEMONIZE_TIME=HH:MM  wait for a specific time before starting command\n"
+    "\tDAEMONIZE_SLEEP=SS    wait for SS seconds before starting command\n"
+    , program
+    );
+  exit(exitcode);
 }
